fffaadde719c3a0ef5fa1bb68ce4deae4abb6fda
Geek Neuron <geekneuron@gmail.com>
2025-07-12 02:38:58 +0330

    Update help.js

--- help.js
+++ help.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
 document.addEventListener('contextmenu', event => event.preventDefault());
-  const { settings } = await chrome.storage.local.get('settings');
+  const { settings } = await browser.storage.local.get('settings');
   if (settings && settings.darkMode) {
     document.body.classList.add('dark-theme');
   }
