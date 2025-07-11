e3687459df75005f2fa0ed127fef37cd2b1702ec
Geek Neuron <geekneuron@gmail.com>
2025-07-12 01:15:49 +0330

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('contextmenu', event => event.preventDefault());
   const { settings } = await chrome.storage.local.get('settings');
   if (settings && settings.darkMode) {
     document.body.classList.add('dark-theme');
