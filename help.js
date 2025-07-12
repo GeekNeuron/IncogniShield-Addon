73e4a7179fcc4424c2eb0fb730e42870596d301a
Geek Neuron <geekneuron@gmail.com>
2025-07-12 03:35:52 +0330

    Delete help.js

--- help.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-document.addEventListener('contextmenu', event => event.preventDefault());
-  const { settings } = await browser.storage.local.get('settings');
-  if (settings && settings.darkMode) {
-    document.body.classList.add('dark-theme');
-  }
-});
